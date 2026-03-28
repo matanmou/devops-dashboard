c1d90dab73006cceba3c2385f55b23f397b9ea0a
matanmou <matanmo93@gmail.com>
2026-03-28 19:28:28 +0300

    add main.py file

---
+++ main.py
@@ -0,0 +1,21 @@
+while True:
+        user_choise = int(input("""
+MAIN MENU
+---------------
+[1] System Info
+[2] Log Checker
+[3] Task List
+[0] Exit
+---------------
+Enrer your choise: """))
+
+        if user_choise == 1:
+                print("Coming soon...")
+        elif user_choise == 2:
+                print("Coming soon...")
+        elif user_choise == 3:
+                print("Coming soon...")
+        elif user_choise == 0:
+                break
+        else:
+                print("Wrong Input!")
