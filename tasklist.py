4b858cfd70ce49e142dd599161c7592ebbb4c5e9
matanmou <matanmo93@gmail.com>
2026-03-28 23:33:44 +0300

    if forgot to add comments to tasklist.py at first

--- tasklist.py
+++ tasklist.py
@@ -1,7 +1,11 @@
+#this module is task manager for devops dashboard
 def manage_tasks():
- tasks_list = []
+"""
+task manager menu
+"""
+ tasks_list = [] #init the task list
  print("=== TASK MANAGER ===")
- while True:
+ while True: #run till user choose to exit
   try:
    user_choise = int(input("""\n[1] Add a task
 [2] View all tasks
@@ -22,18 +26,36 @@ Enter your chiose: """))
    print("Wrong Input! You should enter an number!")
 
 def add_task(task_list):
+"""
+add user's task to task list
+
+Parameters:
+  task_list (list): task list
+
+Return:
+  (list): the new task ist with the new task"
+"""
   task  = input("Enter new task: ")
   task_list.append(task)
   return task_list
 
 def view_all_tasks(task_list):
+"""
+view at all the tasks in task list in a list with numbers
+
+Parameters:
+  task_list (list): task list
+
+Return:
+  (Boolean): if the list is empty or not"
+"""
   print("All tasks list:")
   try:
-    if len(task_list) > 0:
+    if len(task_list) > 0: #if the list is not empty
      i = 0
      for task in task_list:
-      print(f'[{i+1}] - {task}')
-    else:
+      print(f'[{i+1}] - {task}') #print task
+    else: #if the list is empty
       print("The list is empty!")
       return False
   except AttributeError:
@@ -45,12 +67,21 @@ def view_all_tasks(task_list):
   return True
 
 def remove_task(task_list):
+"""
+remove task from task list
+
+Parameters:
+  task_list (list): task list
+
+Return:
+  (list): the new task list without the task that user choose"
+"""
   print("=== Remove Task ===")
-  is_not_empty = view_all_tasks(task_list)
-  if is_not_empty:
+  is_not_empty = view_all_tasks(task_list) #show task list
+  if is_not_empty: #if the list is not empty proccess to remove 
    try:
      user_choise = int(input("Which task to remove (please enter task number): "))
-     task_list.pop(user_choise-1)
+     task_list.pop(user_choise-1) #the remove
      print("Successfully removed!")
      return task_list
    except ValueError:
@@ -59,5 +90,5 @@ def remove_task(task_list):
    except IndexError:
      print("You picked wrong number! please choose from the list!")
      return task_list
-  else:
+  else: #if the list is empty, return the empty list
    return task_list
